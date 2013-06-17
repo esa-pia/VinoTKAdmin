42d0e754fc09bf53bf8a35b53cb7c816c97ca74b
Eric Sampaio <esa@pia-conseil.com>
2013-06-17 13:37:20 +0200

    fix french language

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module VinoTKAdmin
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
-
+    #Adding the below makes it work as expected at heroku
+    config.i18n.locale = :fr 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
