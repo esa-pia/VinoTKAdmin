1c87edc22b38cb41965949ce8ca1663f08ed97c8
Eric Sampaio <esa@pia-conseil.com>
2013-07-12 14:55:55 +0200

    add fix creation catalogue

--- db/migrate/20130712121820_remove_columns_to_bouteuilles_catalogues_table.rb
+++ db/migrate/20130712121820_remove_columns_to_bouteuilles_catalogues_table.rb
@@ -1,7 +1,7 @@
 class RemoveColumnsToBouteuillesCataloguesTable < ActiveRecord::Migration
   def up
-  	remove_column :bouteilles_catalogues, :created_at, :created_at
-    remove_column :bouteilles_catalogues, :updated_at, :updated_at
+  	#remove_column :bouteilles_catalogues, :created_at, :datetime
+    #remove_column :bouteilles_catalogues, :updated_at, :datetime
   end
 
   def down
