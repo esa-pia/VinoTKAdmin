e237d60bbf725a807a96baf9bc86c467cde965c6
Eric Sampaio <esa@pia-conseil.com>
2013-07-29 20:17:40 +0200

    add evenement

--- db/migrate/20130729112252_change_column_description_from_string_to_text_in_table_evenements.rb
+++ db/migrate/20130729112252_change_column_description_from_string_to_text_in_table_evenements.rb
@@ -1,10 +1,10 @@
 class ChangeColumnDescriptionFromStringToTextInTableEvenements < ActiveRecord::Migration
   def up
-	remove_column :evenements, :description, :string
+	remove_column :evenements, :description
     add_column :evenements, :description, :text
   end
   def down
-    remove_column :evenements, :description, :text
+    remove_column :evenements, :description
     add_column :evenements, :description, :string
   end
 end
